2d7140b19f7511b4a1fec9c6613a298950797f61
Anas Siddiqui <anas.siddiqui2204@gmail.com>
2016-04-24 21:52:30 -0400

    Added TreeNode and TreeAPI

---
+++ src/dfs_api/StorageNode.java
@@ -0,0 +1,15 @@
+package dfs_api;
+
+/**
+ * Created by Anas on 4/24/2016.
+ */
+public class StorageNode {
+    public String IPAddr;
+    public String DataNodeID;
+
+    StorageNode(String IPAddr, String DataNodeID)
+    {
+        this.IPAddr = IPAddr;
+        this.DataNodeID = DataNodeID;
+    }
+}
